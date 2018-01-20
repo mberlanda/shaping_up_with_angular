86b29db7dbbf2091f4fb6bce828e9e423cc2b138
Mauro Berlanda <mauro.berlanda@gmail.com>
2018-01-20 22:42:03 +0100

    challenge(2.9): Creating Gallery Controller

--- challenge/app.js
+++ challenge/app.js
@@ -2,6 +2,13 @@
 
   var app = angular.module('gemStore', []);
 
+  app.controller('GalleryController', function(){
+    this.current = 0;
+    this.setCurrent = function(c){
+      this.current = c || 0;
+    };
+  });
+
   app.controller('StoreController', function(){
     this.products = gems;
   });

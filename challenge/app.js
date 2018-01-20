6f142c4b66d9921d002372401bb33b51f023488d
Mauro Berlanda <mauro.berlanda@gmail.com>
2018-01-20 19:20:30 +0100

    challenge(1.7): Look, More Gems!

--- challenge/app.js
+++ challenge/app.js
@@ -1,14 +1,15 @@
 (function(){
-  var gem = {
-    name: 'Azurite',
-    price: 110.50,
-    canPurchase: false,
-    soldOut: true
-  };
+
   var app = angular.module('gemStore', []);
 
   app.controller('StoreController', function(){
-    this.product = gem;
+    this.products = gems;
   });
+
+  var gems = [
+    { name: 'Azurite', price: 2.95 },
+    { name: 'Bloodstone', price: 5.95 },
+    { name: 'Zircon', price: 3.95 }
+  ];
 
 })();
